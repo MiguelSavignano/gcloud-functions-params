7732858b3330af891c336bbd161cb552bc0eb05a
Miguel Savignano <migue.masx@gmail.com>
2018-09-19 00:10:11 +0200

    update package url

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     description="package for manage params in python gcloud functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/MiguelSavignano/gcloud-functions-params",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
